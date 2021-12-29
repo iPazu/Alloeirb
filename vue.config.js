4d124e69ee93de4f776d32858a1da778b9170830
root <root@vps-2a7925fc.vps.ovh.net>
2021-12-29 14:23:59 +0000

    Changed for prod server

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 
 module.exports = {
+devServer: {
+        disableHostCheck: true,
+        port: 4000,
+        public: '0.0.0.0:4000'
+    },
+    publicPath: "/",
 
     transpileDependencies: [
       'vuetify'
