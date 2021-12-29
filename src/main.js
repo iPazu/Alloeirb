ce1ca87b97ebbd152f12cfb99904ae35ab97ea72
LABOIRIE Alex <Alex.Laboirie@bordeaux-inp.fr>
2021-12-29 14:42:02 +0100

    Reverting to mapbox

--- src/main.js
+++ src/main.js
@@ -3,19 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import * as VueGoogleMaps from 'vue2-google-maps'
 
 
 
 Vue.config.productionTip = false
 
-
-Vue.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyAqBOwFWqKMvBX7z8WjbL25Khut2yNO1bA',
-    },
-    installComponents: true
-})
 new Vue({
     router,
     store,
